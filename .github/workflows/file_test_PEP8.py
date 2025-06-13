b536abb3aec4985c38079d555f972268192f8606
Ubuntu <nte.palumbo@gmail.com>
2025-06-13 07:58:13 +0000

    control_test avec correction PEP8

--- .github/workflows/file_test_PEP8.py
+++ .github/workflows/file_test_PEP8.py
@@ -1,10 +1,9 @@
-import pytest
-
 def test_calc_addition():
     # Fonction test du résultat de 2+4
 
     output = 2 + 4
     assert output == 6
+
 
 def test_calc_subtraction():
     # Fonction test du résultat de 2-4
@@ -12,11 +11,13 @@ def test_calc_subtraction():
     output = 2 - 4
     assert output == -2
 
+
 def test_calc_multiply():
     # Fonction test du résultat de 2*4
 
     output = 2 * 4
     assert output == 8
+
 
 def test_coucou():
     # Fonction test si le résultat renvoie 'hello'
